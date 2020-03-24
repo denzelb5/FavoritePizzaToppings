ec3f15c4305b7f204ebbbbb1665b7f6ddf014155
denise baker <denviol@yahoo.com>
2020-03-24 10:13:19 -0500

    Add project files.

---
+++ FavoritePizzaToppings/Pizzas.cs
@@ -0,0 +1,29 @@
+﻿using System;
+using System.Collections.Generic;
+using System.IO;
+using System.Text;
+using System.Text.Json;
+
+namespace FavoritePizzaToppings
+{
+    class Pizzas
+    {
+        public List<string> Toppings { get; set; }
+        public string Name { get; set; }
+
+
+        //   public static object JsonConvert { get; private set; }
+        //   {
+
+
+        //}
+    }
+}
+
+        
+            // deserialize JSON directly from a file
+                //using (StreamReader file = File.OpenText(@"./pizzas.json"))
+                
+        //            JsonSerializer serializer = new JsonSerializer();
+        //var pizza2 = (Pizzas)serializer.Deserialize(file, typeof(Pizzas));
+         
